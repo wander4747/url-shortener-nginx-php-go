05f3c4f4ce248150c71c083d9550a278a6c144cf
wander4747 <wander.douglas14@gmail.com>
2021-09-13 19:31:43 -0300

    create auxiliar request

--- app/index.php
+++ app/index.php
@@ -1,2 +1,29 @@
 <?php
-phpinfo();
+
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+$payload = json_encode(array("link"=> $faker->url));
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'app-go:1234',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => $payload,
+    CURLOPT_HTTPHEADER => array(
+        'Accept: application/json',
+        'Content-Type: application/json'
+    ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
